82913d77a396eccbb23c0deaa0854c1d269cb6be
AndresM <jhoanmendez06@gmail.com>
2024-11-26 19:57:26 -0500

    Se agregaron unos modelos

---
+++ models/Renta.php
@@ -0,0 +1,63 @@
+<?php
+    // Nombra la clase
+    class Renta {
+        
+        // Atributos
+        private $dbh;
+        private $rent_id;
+        private $rent_date;
+        private $rent_date_refund;
+        private $rent_status;
+         // Sobrecarga de Constructores
+         public function __construct(){
+            try {
+                $this->dbh = DataBase::connection();
+                $a = func_get_args();
+                $i = func_num_args();
+                if (method_exists($this, $f = '__construct' . $i)) {
+                    call_user_func_array(array($this, $f), $a);
+                }
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        }
+         # Constructor 04 parámetros
+         public function __construct7($rent_id,$rent_date,$rent_date_refund,$rent_status){
+            $this->rent_id = $rent_id;            
+            $this->rent_date = $rent_date;            
+            $this->rent_date_refund = $rent_date_refund;            
+            $this->rent_status = $rent_status;  
+         }
+         // Métodos setter y getter
+        
+        # Renta: Id
+        public function setRentId($rent_id){
+            $this->rent_id = $rent_id;
+        }
+        public function getRentId(){
+            return $this->rent_id;
+        }
+        # Renta: Fecha         
+        public function setRentDate($rent_date){
+            $this->rent_date =$rent_date = $rent_date;
+        }
+        public function getRentDate(){
+            return $this->rent_date;
+        }
+        # Renta: Fecha devolucion         
+        public function setRentDateRefund($rent_date_refund){
+            $this->rent_date_refund = $rent_date_refund;
+        }
+        public function getUserEmail(){
+            return $this->rent_date_refund;
+        }
+        # Renta: Estado        
+        public function setRentStatus($rent_status){
+            $this->rent_status = $rent_status;
+        }
+        public function getUserPassword(){
+            return $this->rent_status;
+        }
+        
+    }
+  
