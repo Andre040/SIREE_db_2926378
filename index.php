5326287eea43104bb6cfdc5a861d2e0cafb90e47
AndresM <jhoanmendez06@gmail.com>
2024-11-29 20:44:24 -0500

    Se ARREGLO el registro de usuario

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php 
-require_once "views/company/index.view.php";
-require_once "views/company/header.php";
-require_once "views/company/footer.php";
+require_once "views/company/registrarse.php";
 ?>
