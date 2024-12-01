a9a8f4ebfb276d70c0f14c48147ee7c32a0c9255
WilmerCamargo <wilmercamargo14@gmail.com>
2024-12-01 15:22:38 -0500

    Se modifican y corriguen y agregan cosas en decoración.css, Registrarse.js, registrarse.php

--- views/company/registrarse.php
+++ views/company/registrarse.php
@@ -6,33 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer trabajo</title>
     <link rel="stylesheet" href="././assets/Landing/css/decoración.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11">
-    </script>
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
     <section class="Estilo">
         <h4>Registrarse</h4>
-        <form
-            id="F">
-            <input class="Caja" type="text" name="Nombres" id="Nombres" placeholder="Ingrese su nombre" onfocus="ocultar()">
-            <input class="Caja" type="email" name="correo" id="correo" placeholder="Ingrese su correo" onfocus="ocultar()">
-            <input class="Caja" type="password" name="clave" id="clave" placeholder="Ingrese su contraseña" onfocus="mensaje()"> </input>
+        <form id="F" onsubmit="return Alert(event)">
+            <input class="Caja" type="text" name="Nombres" id="Nombres" placeholder="Ingrese su nombre"
+                onfocus="ocultar()" autofocus>
+            <input class="Caja" type="email" name="correo" id="correo" placeholder="Ingrese su correo"
+                onfocus="mensaje('correo')" onfocus="ocultar()"> </input>
+            <h5 id="mensajeCorreo">Solo se permiten los dominios (com, co, com.co, net, es, yahoo)</h5>
+            <input class="Caja" type="password" name="clave" id="clave" placeholder="Ingrese su contraseña"
+                onfocus="mensaje('clave')"> </input>
             <h5 id="mensajeclave">La contraseña debe tener almenos 7 caracteres y solo letras</h5>
-            <input class="Caja" type="password" name="clave2" id="clave2" placeholder="Confirmar contraseña" onfocus="ocultar()"></input>
-           <input placeholder="Tipo de documento" list="Documento" class="Caja" id="DC" onfocus="ocultar()"></input>
-            <input class="Caja" type="number" name="Id" id="Id" placeholder="Numero de documento" onfocus="ocultar()">
-            
-            
-            <p>Estoy de acuerdo con <a href="" class="Enlace">Términos y condiciones</a>
-            <input class="confirmar" type="checkbox" name="check" id="confirmar"> </p>
+            <input class="Caja" type="password" name="clave2" id="clave2" placeholder="Confirmar contraseña"
+                onfocus="ocultar()"></input>
+            <input class="Caja" type="text" name="Rol" id="Rol" placeholder="Ingrese su rol" onfocus="ocultar()">
+            <input class="Caja" type="text" name="Celular" id="Celular" placeholder="Ingrese su Celular"
+                onfocus="ocultar()">
+            <input class="Caja" type="text" name="Direccion" id="Direccion" placeholder="Ingrese su dirección"
+                onfocus="ocultar()">
+            <p>Estoy de acuerdo con <a href="" class="Enlace" target="_blank">Términos y condiciones</a>
+                <input class="confirmar" type="checkbox" name="check" id="confirmar">
+            </p>
             <a href="inicar.html" class="Enlace">Iniciar sección</a>
-            <button class="botones" onclick="Alert()" type="boton" onfocus="ocultar()">Insertar</button>
+            <button class="botones" type="submit" onfocus="ocultar()">Insertar</button>
         </form>
     </section>
-    <script src="././assets/Landing/js/Registrarse.JS">
-    </script>
+    <script src="././assets/Landing/js/Registrarse.JS"></script>
 </body>
 
 </html>
